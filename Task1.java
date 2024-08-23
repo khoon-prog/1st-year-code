198f33c55b71b7a16fc141ac0865a372f3292189
Khan <151509428+khoon-prog@users.noreply.github.com>
2024-08-23 11:44:20 +0800

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    public static  void main(String[] args){
+        System.out.println("x\t\t\tx\nx\t\t\tx\nx\txxxxxxxxxx\tx\nxxxxx\tx\t\sx\s\sxxxxx\nx\s\s\sx\s\s\sx\t\sx\s\sx\s\s\sx\nx\s\s\sx\s\s\sx\t\sx\s\sx\s\s\sx");
+    }
+}
+/*Name ninyo
+  BSIT - 2F*/
